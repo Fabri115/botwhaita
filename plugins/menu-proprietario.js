3576a2a29b5cfbde3ae0a960eedddb4755a1283f
Fabri115 <130554115+Fabri115@users.noreply.github.com>
2023-04-12 12:10:24 +0200

    Add files via upload

---
+++ plugins/menu-proprietario.js
@@ -0,0 +1,56 @@
+import os from 'os'
+import util from 'util'
+import sizeFormatter from 'human-readable'
+import MessageType from '@adiwajshing/baileys'
+import fs from 'fs'
+import { performance } from 'perf_hooks'
+let handler = async (m, { conn, usedPrefix }) => {
+let _uptime = process.uptime() * 1000
+let uptime = clockString(_uptime) 
+let totalreg = Object.keys(global.db.data.users).length
+const chats = Object.entries(conn.chats).filter(([id, data]) => id && data.isChats)
+const groupsIn = chats.filter(([id]) => id.endsWith('@g.us'))
+const groups = chats.filter(([id]) => id.endsWith('@g.us'))
+const used = process.memoryUsage()
+const { restrict } = global.db.data.settings[conn.user.jid] || {}
+const { autoread } = global.opts
+const grup = './Menu2.jpg'
+let old = performance.now()
+let neww = performance.now()
+let speed = (neww - old).toFixed(5)
+let text = `
+
+┌──⭓ 𝐏𝐑𝐎𝐏𝐑𝐈𝐄𝐓𝐀𝐑𝐈𝐎
+│⭔ .attiva/disabilita restrict
+│⭔ .attiva/disabilita antiprivato
+│⭔ .attiva/disabilita sologruppo
+│⭔ .attiva/disabilita autoread
+│⭔ .banuser @
+│⭔ .unbanuser @
+│⭔ .block @
+│⭔ .unblock @
+│⭔ .join
+│⭔ .out
+│⭔ .spamgp (link)
+│⭔ .cleartmp
+│⭔ .blocklist
+│⭔ .banlist
+│⭔ .banchat
+│⭔ .unbanchat
+└───────⭓
+𝐕𝐄𝐋𝐎𝐂𝐈𝐓𝐀': ${speed} 𝐬𝐞𝐜𝐨𝐧𝐝𝐢
+✦✧✧𝐁Ꮻ𝐓 ✧✧✦ 
+`.trim() 
+conn.sendFile(m.chat, grup, 'errop.jpg', text, m, false, )
+}
+handler.help = ['menu']
+handler.tags = ['menu']
+handler.command = /^(menuowner)$/i
+export default handler
+
+function clockString(ms) {
+let h = Math.floor(ms / 3600000)
+let m = Math.floor(ms / 60000) % 60
+let s = Math.floor(ms / 1000) % 60
+console.log({ms,h,m,s})
+return [h, m, s].map(v => v.toString().padStart(2, 0) ).join(':')}
---
+++ plugins/sticker-stickerfilter.js
@@ -0,0 +1,34 @@
+import uploadImage from '../lib/uploadImage.js'
+import { sticker } from '../lib/sticker.js'
+import MessageType from '@adiwajshing/baileys'
+const effects = ['greyscale', 'invert', 'brightness', 'threshold', 'sepia', 'red', 'green', 'blue', 'blurple', 'pixelate', 'blur']
+
+let handler = async (m, { conn, usedPrefix, text }) => {
+let effect = text.trim().toLowerCase()
+if (!effects.includes(effect)) throw `
+uso del comando
+${usedPrefix}stickerfilter (effetto) 
+rispondi a una foto
+Lista effetti
+${effects.map(effect => `_> ${effect}_`).join('\n')}
+`.trim()
+let q = m.quoted ? m.quoted : m
+let mime = (q.msg || q).mimetype || ''
+if (!mime) 
+if (!/image\/(jpe?g|png)/.test(mime)) 
+let img = await q.download()
+let url = await uploadImage(img)
+let apiUrl = global.API('https://some-random-api.ml/canvas/', encodeURIComponent(effect), {
+avatar: url
+})
+try {
+let stiker = await sticker(null, apiUrl, global.packname, global.author)
+conn.sendFile(m.chat, stiker, null, { asSticker: true })
+} catch (e) {
+m.reply('errore')
+await conn.sendFile(m.chat, apiUrl, 'image.png', null, m)
+}}
+handler.help = ['stickfilter (caption|reply media)']
+handler.tags = ['General']
+handler.command = /^(stickerfilter|stikerfilter|cs2)$/i
+export default handler
