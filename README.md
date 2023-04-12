f411f59ac83c97d26543ade69d19df517d3877b8
Fabri115 <130554115+Fabri115@users.noreply.github.com>
2023-04-12 12:05:30 +0200

    Add files via upload

---
+++ README.md
@@ -0,0 +1,73 @@
+# `𝐄ИΞM𝕀Ξ𝐒 🛡️⃟🏴‍☠️ 𝐁Ꮻ𝐓`
+  
+### `—◉ 💥 ATTIVA SU KOYEB 💥`
+
+[![Attiva su Koyeb](https://www.koyeb.com/static/images/deploy/button.svg)](https://app.koyeb.com/deploy?type=git&repository=github.com/unlimited787/Enemies-md-ofc&branch=master&name=mysticbot)
+  
+### `—◉ 🌌 ATTIVA SU REPLIT 🌌`
+
+[![Attiva su Repl.it](https://repl.it/badge/github/unlimited787/Enemies-md-ofc)](https://repl.it/github/unlimited787/Enemies-md-ofc)  
+
+### `—◉ ⚙️ CONFIGURAZIONE ⚙️`
+- CLONARE LA REPOSITORY [qui](https://github.com/unlimited787/Enemies-md-ofc/fork)
+- CAMBIARE NUMERO DEL PROPRIETARIO [qui](https://github.com/unlimited787/Enemies-md-ofc/blob/master/config.js)
+
+### `—◉ VERSIONE DI TERMUX NECESSARIA: ` 
+https://www.mediafire.com/file/0npdmv51pnttps0/com.termux_0.119.1-119_minAPI21(arm64-v8a,armeabi-v7a,x86,x86_64)(nodpi)_apkmirror.com.apk/file
+
+### `—◉ 👾 ATTIVA IN TERMUX 👾` 
+- DIGITA QUESTI COMANDI:
+```bash
+cd && termux-setup-storage
+```
+
+```bash
+apt-get update -y && apt-get upgrade -y
+```
+
+```bash
+pkg install -y git nodejs ffmpeg imagemagick && pkg install yarn
+```
+
+```bash
+git clone https://github.com/unlimited787/Enemies-md-ofc && cd Enemies-md-ofc
+```
+
+```bash
+yarn install
+```
+
+```bash
+npm install
+```
+
+```bash
+npm update
+```
+
+```bash
+sh start.sh
+```
+
+### `—◉ ✔️ AVVIA IN TERMUX ✔️`
+```bash
+DIGITA QUESTI COMANDI:
+> cd 
+> cd Enemies-md-ofc
+> sh start.sh
+```
+
+### `—◉ 👽 OTTIENI UN ALTRO CODICE QR 👽`
+```bash
+DIGITA QUESTI COMANDI:
+> cd 
+> cd Enemies-md-ofc
+> rm -rf MysticSession
+> sh start.sh
+```
+
+### `—◉ 📝 NOTE 📝`
+- IL BOT E' COMPATIBILE CON WHATSAPP BUSINESS E WHATSAPP NORMALE
+- PER BLOCCARE GLI UTENTI IN PRIVATO ESEGUI IL COMANDO .attiva antiprivato
+- ALL'AVVIO ESEGUI IL COMANDO .attiva restrict DAL NUMERO DEL BOT
+
---
+++ config.js
@@ -0,0 +1,105 @@
+import { watchFile, unwatchFile } from 'fs'
+import chalk from 'chalk'
+import { fileURLToPath } from 'url'
+
+global.owner = [
+  ['393518398856', 'nobody|', true]
+
+] //Numeros de owner 
+
+//━━━━━━━━━━━━━━━━━━━━ ฅ^•ﻌ•^ฅ
+
+global.mods = ['393518398856'] 
+global.prems = ['393518398856', '13345214029']
+
+//━━━━━━━━━━━━━━━━━━━━ ฅ^•ﻌ•^ฅ
+
+global.keysZens = ['c2459db922', '37CC845916', '6fb0eff124']
+// 'fiktod' 'BF39D349845E' '675e34de8a' '0b917b905e6f'
+global.keysxxx = keysZens[Math.floor(keysZens.length * Math.random())]
+global.keysxteammm = ['29d4b59a4aa687ca', '5LTV57azwaid7dXfz5fzJu', 'cb15ed422c71a2fb', '5bd33b276d41d6b4', 'HIRO', 'kurrxd09', 'ebb6251cc00f9c63']
+global.keysxteam = keysxteammm[Math.floor(keysxteammm.length * Math.random())]
+global.keysneoxrrr = ['5VC9rvNx', 'cfALv5']
+global.keysneoxr = keysneoxrrr[Math.floor(keysneoxrrr.length * Math.random())]
+global.lolkeysapi = ['BrunoSobrino']
+
+global.APIs = { 
+  xteam: 'https://api.xteam.xyz', 
+  nrtm: 'https://fg-nrtm-nhie.onrender.com',
+  bg: 'http://bochil.ddns.net',
+  fgmods: 'https://api-fgmods.ddns.net',
+  dzx: 'https://api.dhamzxploit.my.id',
+  lol: 'https://api.lolhuman.xyz',
+  violetics: 'https://violetics.pw',
+  neoxr: 'https://api.neoxr.my.id',
+  zenzapis: 'https://zenzapis.xyz',
+  akuari: 'https://api.akuari.my.id',
+  akuari2: 'https://apimu.my.id',	
+  fgmods: 'https://api-fgmods.ddns.net'
+},
+global.APIKeys = { 
+  'https://api.xteam.xyz': `${keysxteam}`,
+  'https://api.lolhuman.xyz': '85faf717d0545d14074659ad',
+  'https://api.neoxr.my.id': `${keysneoxr}`,	
+  'https://violetics.pw': 'beta',
+}
+
+global.imagen1 = ['./media/menu1.jpg']
+
+//━━━━━━━━━━━━━━━━━━━━ ฅ^•ﻌ•^ฅ
+
+// Sticker WM
+global.packname = 'SUPERFABRIBOT-MD'
+global.author = 'SUPERFABRIBOT-MD'
+
+//━━━━━━━━━━━━━━━━━━━━ ฅ^•ﻌ•^ฅ
+
+global.fgig = 'https://github.com/fabriziodee/superbot2023'
+ 
+global.yt = ''
+
+global.fgpyp = 'https://www.tiktok.com/@3r1z10'
+
+global.faceb = ''
+
+global.vs = 'Mi0 2.1'
+
+global.lb = 'SUPERFABRIBOT-MD'
+global.lolibot = lb
+
+global.dygp = 'https://chat.whatsapp.com/BHZDSUDHuwWEed4Ut8aeUQ' //grupo del bot
+
+global.nna = 'https://chat.whatsapp.com/BHZDSUDHuwWEed4Ut8aeUQ' //UPDATE
+
+global.fglog = 'https://telegra.ph/file/1861aab98389b13db8588.png' 
+
+global.multiplier = 69 
+global.maxwarn = '4' // máxima advertencias
+
+//━━━━━━━━━━━━━━━━━━━━ ฅ^•ﻌ•^ฅ
+
+global.wm = 'SUPERFABRIBOT-MD'
+global.wait = '*⌛ _Caricando..._*\n*▰▰▰▱▱▱▱▱*'
+global.rwait = '⌛'
+global.dmoji = '🤭'
+global.done = '✅'
+global.error = '❌' 
+global.xmoji = '🔥' 
+
+//━━━━━━━━━━━━━━━━━━━━ ฅ^•ﻌ•^ฅ
+
+global.flaaa = [
+'https://flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=water-logo&script=water-logo&fontsize=90&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&fillTextColor=%23000&shadowGlowColor=%23000&backgroundColor=%23000&text=',
+'https://flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=crafts-logo&fontsize=90&doScale=true&scaleWidth=800&scaleHeight=500&text=',
+'https://flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=amped-logo&doScale=true&scaleWidth=800&scaleHeight=500&text=',
+'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=sketch-name&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&fillTextType=1&fillTextPattern=Warning!&text=',
+'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=sketch-name&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&fillTextType=1&fillTextPattern=Warning!&fillColor1Color=%23f2aa4c&fillColor2Color=%23f2aa4c&fillColor3Color=%23f2aa4c&fillColor4Color=%23f2aa4c&fillColor5Color=%23f2aa4c&fillColor6Color=%23f2aa4c&fillColor7Color=%23f2aa4c&fillColor8Color=%23f2aa4c&fillColor9Color=%23f2aa4c&fillColor10Color=%23f2aa4c&fillOutlineColor=%23f2aa4c&fillOutline2Color=%23f2aa4c&backgroundColor=%23101820&text=']
+
+//━━━━━━━━━━━━━━━━━━━━ ฅ^•ﻌ•^ฅ
+
+let file = fileURLToPath(import.meta.url)
+watchFile(file, () => {
+  unwatchFile(file)
+  console.log(chalk.redBright("Update 'config.js'"))
+  import(`${file}?update=${Date.now()}`)
+})
