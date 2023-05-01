180586e2a81fb0b411a9f4e138e53def4d4a0770
F S <130554115+Fabri115@users.noreply.github.com>
2023-05-01 09:10:58 +0200

    Update Charlie-1.js

--- plugins/Charlie-1.js
+++ plugins/Charlie-1.js
@@ -1,67 +1,11 @@
-let handler  = async (m, { conn }) => { 
-   conn.reply(m.chat,` *Fabri ha detto* \n*“${pickRandom(global.charlie)}”*\n   
-   `, m) 
- } 
-
- handler.help = ['si o no']
-handler.tags = ['si o no']
-handler.customPrefix = /si|no/i
-handler.command = new RegExp
 
 
 
-  
 
- export        default handler 
 
-  
 
- function pickRandom(list) { 
 
-   return list[Math.floor(list.length * Math.random())] 
 
- } 
 
-  
 
- // charlie charlie by odin el fucker kkkkkkkkkk 
 
- global.charlie = [ 
-
- "Si, puoi","No, non puoi","Si, puoi","No, non puoi","Si, puoi","No, non puoi","Si, puoi","No, non puoi","Si, puoi","No, non puoi",  
-
- "Si, puoi","No, non puoi","Si, puoi","No, non puoi","Si, puoi","No, non puoi","Si, puoi","No, non puoi","Si, puoi","No, non puoi",   
-
- "Si, puoi","No, non puoi","Si, puoi","No, non puoi","Si, puoi","No, non puoi","Si, puoi","No, non puoi","Si, puoi","No, non puoi",  
-
- "Si, puoi","No, non puoi","Si, puoi","No, non puoi","Si, puoi","No, non puoi","Si, puoi","No, non puoi","Si, puoi","No, non puoi",   
-
- "Si, puoi","No, non puoi","Si, puoi","No, non puoi","Si, puoi","No, non puoi","Si, puoi","No, non puoi","Si, puoi","No, non puoi",   
-
- "Si, puoi","No, non puoi","Si, puoi","No, non puoi","Si, puoi","No, non puoi","Si, puoi","No, non puoi","Si, puoi","No, non puoi",   
-
- "Si, puoi","No, non puoi","Si, puoi","No, non puoi","Si, puoi","No, non puoi","Si, puoi","No, non puoi","Si, puoi","No, non puoi",   
-
- "Si, puoi","No, non puoi","Si, puoi","No, non puoi","Si, puoi","No, non puoi","Si, puoi","No, non puoi","Si, puoi","No, non puoi",   
-
- "Si, puoi","No, non puoi","Si, puoi","No, non puoi","Si, puoi","No, non puoi","Si, puoi","No, non puoi","Si, puoi","No, non puoi",   
-
- "Si, puoi","No, non puoi","Si, puoi","No, non puoi","Si, puoi","No, non puoi","Si, puoi","No, non puoi","Si, puoi","No, non puoi",   
-
- "Si, puoi","No, non puoi","Si, puoi","No, non puoi","Si, puoi","No, non puoi","Si, puoi","No, non puoi","Si, puoi","No, non puoi",   
-
- "Si, puoi","No, non puoi","Si, puoi","No, non puoi","Si, puoi","No, non puoi","Si, puoi","No, non puoi","Si, puoi","No, non puoi",   
-
- "Si, puoi","No, non puoi","Si, puoi","No, non puoi","Si, puoi","No, non puoi","Si, puoi","No, non puoi","Si, puoi","No, non puoi",   
-
- "Si, puoi","No, non puoi","Si, puoi","No, non puoi","Si, puoi","No, non puoi","Si, puoi","No, non puoi","Si, puoi","No, non puoi",   
-
- "Si, puoi","No, non puoi","Si, puoi","No, non puoi","Si, puoi","No, non puoi","Si, puoi","No, non puoi","Si, puoi","No, non puoi",   
-
- "Si, puoi","No, non puoi","Si, puoi","No, non puoi","Si, puoi","No, non puoi","Si, puoi","No, non puoi","Si, puoi","No, non puoi",   
-
- "Si, puoi","No, non puoi","Si, puoi","No, non puoi","Si, puoi","No, non puoi","Si, puoi","No, non puoi","Si, puoi","No, non puoi",   
-
- "Si, puoi","No, non puoi","Si, puoi","No, non puoi","Si, puoi","No, non puoi","Si, puoi","No, non puoi","Si, puoi","No, non puoi"
-
- ] 
