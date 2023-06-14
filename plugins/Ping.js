a8f9879edf48c38d63a5d54dd4cad8a8fb2a5493
F S <130554115+Fabri115@users.noreply.github.com>
2023-06-14 21:41:58 +0200

    Delete Ping.js

--- plugins/Ping.js
+++
@@ -1,9 +0,0 @@
-import fs from 'fs'
-function handler(m, { conn }) {
-let text = `pong frocetto`.trim()   
-conn.reply(m.chat, text, m, {
-})
-}
-handler.customPrefix = /(ping)/i
-handler.command = new RegExp
-export default handler
