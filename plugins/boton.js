86aabc582dfc1dc889b91d5961c9b763d70871c6
F S <130554115+Fabri115@users.noreply.github.com>
2023-07-04 07:59:12 +0200

    Delete plugins directory

--- plugins/boton.js
+++
@@ -1,8 +0,0 @@
-import fs from 'fs'
-function handler(m, { conn }) {
-let text = `sesso 🎶sesso 🎶sesso 🎶sesso Amnesia Bot si sta avviando, se vedete che fa cazzate non toglietelo brutti froci perchè si sta aggiornando Sesso🎶 sesso 🎶sesso🎶 sesso`.trim()   
-conn.reply(m.chat, text, m, {
-})
-}
-handler.command = /^(boton)/i
-export default handler
--- plugins/owner-leavegc.js
+++
@@ -1,7 +0,0 @@
-let handler = async (m, { conn, args, command }) => {
-await m.reply('𝐛𝐲𝐞 𝐛𝐲𝐞👋') 
-await  conn.groupLeave(m.chat)}
-handler.command = /^(out|leavegc|leave|salirdelgrupo)$/i
-handler.group = true
-handler.rowner = true
-export default handler
