133448141b16a862a9904cae2ca9ca914adde806
F S <130554115+Fabri115@users.noreply.github.com>
2023-07-05 12:40:38 +0200

    ρℓυgιη вσтωнαιтα є∂ιт ƒαвяι115©

---
+++ plugins/STRUMENTI_openai.js
@@ -0,0 +1,13 @@
+import fetch from 'node-fetch'
+let handler = async (m, { text,  usedPrefix,  command }) => {
+    if (!text) throw `dica??`
+await m.reply('aspe...')
+let zykomd = await fetch(`https://api.lolhuman.xyz/api/openai?apikey=SGWN&text=${text}&user=user-unique-id`)
+let hasil = await zykomd.json()
+ m.reply(`${hasil.result}`.trim())
+    }  
+handler.help = ['ai', 'openai']
+handler.tags = ['fun']
+handler.command = /^(aii)$/i
+export default handler
+
