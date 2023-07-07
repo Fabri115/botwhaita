88aad0f4319d7cb551b49d21220fd992b8d872ce
F S <130554115+Fabri115@users.noreply.github.com>
2023-07-07 13:34:16 +0200

    Update INFO_installami.js

--- plugins/INFO_installami.js
+++ plugins/INFO_installami.js
@@ -1,5 +1,6 @@
 import fs from 'fs'
 let handler  = async (m, { conn, command, args, usedPrefix, DevMode }) => {
+let sticker = `./Media/Video/bot.mp4`
 let text = `
 𝐂𝐨𝐦𝐚𝐧𝐝𝐢 𝐩𝐞𝐫 𝐓𝐄𝐑𝐌𝐔𝐗 📱
 
@@ -12,22 +13,21 @@ let text = `
 > pkg install ffmpeg -y
 > pkg install imagemagick -y
 > pkg install yarn
-> git clone https://github.com/Fabri115/BotWhaIta.git
-> cd BotWhaIta 
+> git clone https://github.com/Fabri115/BotWhaIta.git o tuo link del fork
+> cd BotWhaIta o tuo nome bot
 > yarn install 
 > npm install
 > npm update
 > npm start
 
 𝐂𝐨𝐦𝐚𝐧𝐝𝐢 𝐩𝐞𝐫 𝐓𝐄𝐑𝐌𝐔𝐗 📱 PER SCARICARE IL BOT
-cd /sdcard && rm -rf BotWhaIta && git clone https://github.com/Fabri115/BotWhaIta.git && cd BotWhaIta && sh start.sh 
+cd /sdcard && rm -rf BotWhaIta && git clone https://github.com/Fabri115/BotWhaIta.git o tuo link del fork && cd BotWhaIta o tuo nome bot && sh start.sh 
+
+Tutorial: https://www.youtube.com/watch?v=6Cg1yUMz-Do
 `.trim()   
-conn.reply(m.chat, text, m, {
-contextInfo: { externalAdReply :{ mediaUrl: null, mediaType: 1, description: null, 
-title: 'bot',
-body: 'bot',         
-previewType: 0, thumbnail: fs.readFileSync("./Menu2.jpg"),
-sourceUrl: `https://github.com/`}}})   
+
+conn.reply(m.chat, text, m,{
+})   
 }
 handler.command = /^(installa)/i
 export default handler
