6374f831db6ed642a77782d414a0634300921e05
F S <130554115+Fabri115@users.noreply.github.com>
2023-07-07 13:42:45 +0200

    ρℓυgιη вσтωнαιтα є∂ιт ƒαвяι115©

--- plugins/INFO_installami.js
+++ plugins/INFO_installami.js
@@ -21,7 +21,26 @@ let text = `
 > npm start
 
 𝐂𝐨𝐦𝐚𝐧𝐝𝐢 𝐩𝐞𝐫 𝐓𝐄𝐑𝐌𝐔𝐗 📱 PER SCARICARE IL BOT
-cd /sdcard && rm -rf BotWhaIta && git clone https://github.com/Fabri115/BotWhaIta.git o tuo link del fork && cd BotWhaIta o tuo nome bot && sh start.sh 
+> cd /sdcard && rm -rf BotWhaIta && git clone https://github.com/Fabri115/BotWhaIta.git o tuo link del fork && cd BotWhaIta o tuo nome bot && sh start.sh 
+
+⚙️ CONFIGURAZIONE 𝑴☁️ ⚙️
+> CLONARE LA REPOSITORY [qui](https://github.com/Fabri115/BotWhaIta/fork)
+>  CAMBIARE NUMERO DEL PROPRIETARIO [qui](https://github.com/Fabri115/BotWhaIta/blob/master/config.js)
+
+💥 ATTIVA SU KOYEB 💥
+> https://app.koyeb.com/deploy?type=git&repository=github.com/Fabri115/BotWhaIta&branch=master&name=BotWhaIta
+
+🌌 ATTIVA 𝑴☁️ SU REPLIT 🌌
+> https://replit.com/new/github/Fabri115/botwhaita
+
+🌌 ATTIVA 𝑴☁️ SU HEROKU 🌌
+> https://heroku.com/deploy?template=https://github.com/Fabri115/botwhaita
+
+VERSIONE DI TERMUX NECESSARIA:
+> https://www.mediafire.com/file/0npdmv51pnttps0/com.termux_0.119.1-119_minAPI21(arm64-v8a,armeabi-v7a,x86,x86_64)(nodpi)_apkmirror.com.apk/file
+
+👽 OTTIENI UN ALTRO CODICE QR 𝑴☁️ 👽
+> rm -rf MysticSession
 
 Tutorial: https://www.youtube.com/watch?v=6Cg1yUMz-Do
 `.trim()   
