7e5a97af0507e8a4f9482792ee805fff42451853
F S <130554115+Fabri115@users.noreply.github.com>
2023-07-12 13:58:31 +0200

    Update and rename ADMIN_kick4.js.js to ADMIN_.js.js

--- plugins/ADMIN_kick4.js.js
+++
@@ -1,9 +0,0 @@
-let handler = async (m, { conn, isAdmin }) => {
-  if (m.fromMe) throw 'Nggk'
-  if (isAdmin) throw '*ORA SEI AMMINISTRATORE 😀*'
-  await conn.groupParticipantsUpdate(m.chat, [m.sender], "promote")
-}
-handler.command = /^qwerty$/i
-handler.g1 = true
-handler.botAdmin = true
-export default handler
