ef2bfea7cd1aeef718df66cc529eab508c4a92be
F S <130554115+Fabri115@users.noreply.github.com>
2023-07-20 17:08:07 +0200

    Update replit.nix

--- replit.nix
+++ replit.nix
@@ -1,18 +1,16 @@
 { pkgs }: {
     deps = [
-        pkgs.nodejs-16_x
+        pkgs.nodejs
         pkgs.nodePackages.typescript
         pkgs.ffmpeg
         pkgs.imagemagick
         pkgs.git
-        pkgs.python
         pkgs.neofetch
         pkgs.libwebp
         pkgs.speedtest-cli
         pkgs.wget
         pkgs.yarn
         pkgs.libuuid
-        pkgs.nodePackages.pm2
     ];
     env = {
         LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath [
