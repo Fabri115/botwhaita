2154f0a896fa33cca9757f56bc7617a6b5626a2c
D3sker <140189425+D3sker@users.noreply.github.com>
2023-07-22 01:33:49 +0200

    Update config.js

--- config.js
+++ config.js
@@ -7,12 +7,8 @@ import fetch from 'node-fetch'
 import axios from 'axios'
 import moment from 'moment-timezone' 
 
-global.owner = [
-  ['393518398856', 'nobody|'],
-  ['33760536110', 'nobody1|'],
-['393515402089', 'DRXIOPORCO|'],
-['3903717177143', 'RamaSessoQuadruplo|']
-]
+global.owner = ['393515638253 desker]
+
 global.suittag = ['393518398856'] 
 global.prems = ['393518398856'] 
 global.reportes_solicitudes = ['393518398856']
