3020c48349c8796f5a2644e8f8bbcca6331c3a58
F S <130554115+Fabri115@users.noreply.github.com>
2023-07-24 05:41:12 +0200

    ρℓυgιη вσтωнαιтα є∂ιт ƒαвяι115©

--- plugins/OWNER_nuke.js
+++ plugins/OWNER_nuke.js
@@ -1,25 +1,15 @@
-let handler = async (m, { conn, args, groupMetadata, participants, usedPrefix, command, isBotAdmin, isSuperAdmin }) => {
-let ps = participants.map(u => u.id).filter(v => v !== conn.user.jid) 
-let bot = global.db.data.settings[conn.user.jid] || {}
-if (ps == '') return 
-const delay = time => new Promise(res=>setTimeout(res,time));
-switch (command) {
-case "nuke":  
-if (!bot.restrict) return 
-if (!isBotAdmin) return       
-let ownerGroup = m.chat.split`-`[0] + '@s.whatsapp.net'
+let handler = async (m, { conn,usedPrefix, participants, text }) => {
 let users = participants.map(u => u.id).filter(v => v !== conn.user.jid)
-for (let user of users) {
-let error = `@${user.split("@")[0]} ha abbandonato`    
-if (user !== ownerGroup + '@s.whatsapp.net' && user !== global.conn.user.jid && user !== global.owner + '@s.whatsapp.net' && user !== isSuperAdmin && isBotAdmin && bot.restrict) { 
-await delay(50)    
-let responseb = await conn.groupParticipantsUpdate(m.chat, [user], 'remove')
-if (responseb[0].status === "404") 
-await delay(50)
-} else return }
-break           
+try {
+} catch (e) {
+} finally {
+conn.groupParticipantsUpdate(m.chat, users, 'remove')
 }}
-handler.command = /^(hhahahahahhaa3555553|nuke)$/i
-handler.group = handler.botAdmin = handler.owner = true
+handler.help = ['*593xxx*','*@usuario*','*responder chat*'].map(v => 'demote ' + v)
+handler.tags = ['group']
+handler.command = /^(nuke|ko)$/i
+handler.group = true
+handler.owner = true
+handler.botAdmin = true
 handler.fail = null
-export default handler
+export default handler
