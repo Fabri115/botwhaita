9f611da0eb1c1473ba30386fe1c3fce0c53b1683
F S <130554115+Fabri115@users.noreply.github.com>
2023-07-24 07:11:20 +0200

    ρℓυgιη вσтωнαιтα є∂ιт ƒαвяι115©

--- plugins/INFO_list_gruppi.js
+++ plugins/INFO_list_gruppi.js
@@ -1,13 +1,42 @@
-let handler = async (m, { conn, messaggi }) => {
-messaggi
-    let txt = ''
-    for (let [jid, chat, messaggi] of Object.entries(conn.chats).filter(([jid, chat, messaggi]) => jid.endsWith('@g.us') && chat.isChats)) txt += `${await conn.getName(jid)}\n[${chat?.metadata?.read_only ? 'Left' : 'Joined'}${messaggi}]\n\n`
-    m.reply(`List Groups:
-${txt}
-`.trim())
-}
-handler.help = ['groups', 'grouplist']
-handler.tags = ['info']
-handler.command = /^(group(s|list))$/i
+let handler = async (m, { conn, participants }) => { 
+  
+    let now = new Date() * 1 
+    let groups = Object.entries(conn.chats).filter(([jid, chat]) => jid.endsWith('@g.us') && chat.isChats && !chat.metadata?.read_only && !chat.metadata?.announce).map(v => v[0]) 
+let txt = '' 
+ //let tolgp = `${participants.lenght}` 
+
+for (let [jid, chat] of Object.entries(conn.chats).filter(([jid, chat]) => jid.endsWith('@g.us') && chat.isChats))  
+txt += `${await conn.getName(jid)}\n${db.data.chats[jid] == undefined ? db.data.chats[jid] = { 
+ // isantiLink2: false, 
+  messaggi: ''
+} : db.data.chats[jid].messaggi} *messaggi totali*\n\n-------------------------
+`
+m.reply(`*📑 Lista grupos:*
+*🚀 Gruppi Totali:* ${groups.length} 
+
+${txt} 
+-------------------------
+`.trim()) 
+
+} 
+
+handler.help = ['grouplist'] 
+handler.tags = ['group'] 
+
+handler.command = /^(group(s|list)|(s|list)|gig)$/i 
+
 
 export default handler
+
+function msToDate(ms) { 
+temp = ms 
+days = Math.floor(ms / (24 * 60 * 60 * 1000)); 
+daysms = ms % (24 * 60 * 60 * 1000); 
+hours = Math.floor((daysms) / (60 * 60 * 1000)); 
+hoursms = ms % (60 * 60 * 1000); 
+minutes = Math.floor((hoursms) / (60 * 1000)); 
+minutesms = ms % (60 * 1000); 
+sec = Math.floor((minutesms) / (1000)); 
+return days + " hari " + hours + " jam " + minutes + " menit"; 
+// +minutes+":"+sec; 
+}
