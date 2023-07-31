54337daa8191b4f421205e07f00f075f5993c3fc
F S <130554115+Fabri115@users.noreply.github.com>
2023-07-31 06:43:16 +0200

    Update CONFIG_antilink.js

--- plugins/CONFIG_antilink.js
+++ plugins/CONFIG_antilink.js
@@ -1,5 +1,5 @@
-let linkRegex = /chat.whatsapp.com\/([0-9A-Za-z]{20,24})/i
-export async function before(m, { isAdmin, isBotAdmin }) {
+let linkRegex = /https:/i
+export async function before(m, { isAdmin, isBotAdmin, text }) {
 if (m.isBaileys && m.fromMe)
 return !0
 if (!m.isGroup) return !1
@@ -8,18 +8,16 @@ let delet = m.key.participant
 let bang = m.key.id
 let bot = global.db.data.settings[this.user.jid] || {}
 const isGroupLink = linkRegex.exec(m.text)
-const grupo = `https://chat.whatsapp.com`
-if (isAdmin && chat.antiLink && m.text.includes(grupo)) return
-if (chat.antiLink && isGroupLink && !isAdmin) {
+if (chat.antiLink2 && isGroupLink && !isAdmin) {
 if (isBotAdmin) {
-const linkThisGroup = `https://chat.whatsapp.com/${await this.groupInviteCode(m.chat)}`
+const linkThisGroup = `chat.whatsapp.com/${await this.groupInviteCode(m.chat)}`
 if (m.text.includes(linkThisGroup)) return !0
 }    
 if (isBotAdmin && bot.restrict) {
 await conn.sendMessage(m.chat, { delete: { remoteJid: m.chat, fromMe: false, id: bang, participant: delet }})
 let responseb = await conn.groupParticipantsUpdate(m.chat, [m.sender], 'remove')
-if (responseb[0].status === "404") return   
-} else if (!bot.restrict) return
+if (responseb[0].status === "404") return
+} else if (!bot.restrict) return 
 }
 return !0
 }
