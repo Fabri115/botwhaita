62815b67ad405d1c898a9a94734a2c5acaf304b6
F S <130554115+Fabri115@users.noreply.github.com>
2023-08-13 12:09:21 +0200

    Delete plugins directory

--- plugins/FUN_pong.js
+++
@@ -1,9 +0,0 @@
-import fs from 'fs'
-function handler(m, { conn }) {
-let text = `ping frocetto`.trim()   
-conn.reply(m.chat, text, m, {
-})
-}
-handler.customPrefix = /(pong)/i
-handler.command = new RegExp
-export default handler
