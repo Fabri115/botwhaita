1569c2fe497e87860f995566770b6c8c61a4c057
F S <130554115+Fabri115@users.noreply.github.com>
2023-08-16 07:03:10 +0200

    Update INFO_tempo_attività.js

--- plugins/INFO_tempo_attività.js
+++ plugins/INFO_tempo_attività.js
@@ -1,28 +1,38 @@
-import fs from 'fs';
-const handler = async (m, {conn, args, command}) => {
-  let _muptime;
-  if (process.send) {
-    process.send('uptime');
-    _muptime = await new Promise((resolve) => {
-      process.once('message', resolve);
-      setTimeout(resolve, 1000);
-    }) * 1000;
+const handler = async (m, {conn}) => {
+  try {
+    const pp = imagen4;
+    const img = await(await fetch('https://github.com/Fabri115.png')).buffer();
+    const _uptime = process.uptime() * 1000;
+    const uptime = clockString(_uptime);
+    const taguser = '@' + m.sender.split('@s.whatsapp.net')[0];
+    const str = `╭────[ *BOTWHAITA* ]
+│
+│ *➤ Ciao ${taguser}*
+│
+│ *=> 🤖 Tempo attività:* ${uptime}
+│ *=> ✅ Bot di uso Pubblico*
+│ *=> 👑 Creatore: Fabri115*
+│ *=> 🔗 link bot:* https://github.com/Fabri115/botwhaita
+╰────────────────`.trim();
+    if (m.isGroup) {
+      conn.sendMessage(m.chat, {text: str.trim(), mentions: [...str.matchAll(/@([0-9]{5,16}|0)/g)].map((v) => v[1] + '@s.whatsapp.net'), contextInfo: {forwardingScore: 9999999, isForwarded: true, mentionedJid: [...str.matchAll(/@([0-9]{5,16}|0)/g)].map((v) => v[1] + '@s.whatsapp.net'), "externalAdReply": {"showAdAttribution": true, "containsAutoReply": true, "renderLargerThumbnail": true, "title": global.titulowm, "containsAutoReply": true, "mediaType": 1, "thumbnail": pp, "mediaUrl": `https://github.com/Fabri115/botwhaita`, "sourceUrl": `https://github.com/Fabri115/botwhaita`}}}, {quoted: m});
+      //conn.sendMessage(m.chat, {image: pp, caption: str.trim(), mentions: [...str.matchAll(/@([0-9]{5,16}|0)/g)].map((v) => v[1] + '@s.whatsapp.net')}, {quoted: m});
+    } else {
+      const fkontak2 = {'key': {'participants': '0@s.whatsapp.net', 'remoteJid': 'status@broadcast', 'fromMe': false, 'id': 'Halo'}, 'message': {'contactMessage': {'vcard': `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD`}}, 'participant': '0@s.whatsapp.net'};
+      conn.sendMessage(m.chat, {text: str.trim(), mentions: [...str.matchAll(/@([0-9]{5,16}|0)/g)].map((v) => v[1] + '@s.whatsapp.net'), contextInfo: {forwardingScore: 9999999, isForwarded: true, mentionedJid: [...str.matchAll(/@([0-9]{5,16}|0)/g)].map((v) => v[1] + '@s.whatsapp.net'), "externalAdReply": {"showAdAttribution": true, "containsAutoReply": true, "renderLargerThumbnail": true, "title": global.titulowm, "containsAutoReply": true, "mediaType": 1, "thumbnail": pp, "mediaUrl": `https://github.com/Fabri115/botwhaita`, "sourceUrl": `https://github.com/Fabri115/botwhaita`}}}, {quoted: fkontak2});
+      //conn.sendMessage(m.chat, {image: pp, caption: str.trim(), mentions: [...str.matchAll(/@([0-9]{5,16}|0)/g)].map((v) => v[1] + '@s.whatsapp.net')}, {quoted: fkontak2});
+    }
+  } catch {
   }
-  const muptime = clockString(_muptime);
-  conn.reply(m.chat, ` *R U N T I M E* \n${muptime}\n`, m, {
-  });
 };
-
-
-handler.help = ['runtime'];
-handler.tags = ['info'];
-handler.command = ['runtime', 'rt'];
+handler.help = ['estado'];
+handler.tags = ['main'];
+handler.command = /^(estado|status|estate|state|stado|stats|runtime|uptime)$/i;
 export default handler;
-
 function clockString(ms) {
   const d = isNaN(ms) ? '--' : Math.floor(ms / 86400000);
   const h = isNaN(ms) ? '--' : Math.floor(ms / 3600000) % 24;
   const m = isNaN(ms) ? '--' : Math.floor(ms / 60000) % 60;
   const s = isNaN(ms) ? '--' : Math.floor(ms / 1000) % 60;
-  return [d, ' *Days ☀️*\n ', h, ' *Hours 🕐*\n ', m, ' *Minute ⏰*\n ', s, ' *Second ⏱️* '].map((v) => v.toString().padStart(2, 0)).join('');
+  return [`\n│ *=> 💥 ` + d, ' Giorno(i)* ', `\n│ *=> 💫 ` + h, ' Ora(e)* ', `\n│ *=> 💠 ` + m, ' Minuto(i)* ', `\n│ *=> ♦ ` + s, ' Secondo(i)* '].map((v) => v.toString().padStart(2, 0)).join('');
 }
